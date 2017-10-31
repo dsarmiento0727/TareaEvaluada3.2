28914d0aae8a18d7d9b9a1f0c77da1b473aa78f5
David Sarmiento <David Sarmiento@192.168.1.9>
2017-10-31 00:33:48 -0600

    Re haciendo Bean

--- src/java/com/beans/BeanDepartamento.java
+++ src/java/com/beans/BeanDepartamento.java
@@ -8,6 +8,8 @@ package com.beans;
 import com.dao.DaoDepartamento;
 import com.modelo.Departamento;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
 
@@ -19,12 +21,52 @@ import javax.faces.view.ViewScoped;
 @ViewScoped
 public class BeanDepartamento implements Serializable{
     private Departamento d = new Departamento();
+    private Departamento selDepto = new Departamento();
     private DaoDepartamento ddp = new DaoDepartamento();
+    private List<Departamento> lista = new ArrayList<>();
+
+    public Departamento getD() {
+        return d;
+    }
+
+    public void setD(Departamento d) {
+        this.d = d;
+    }
+
+    public Departamento getSelDepto() {
+        return selDepto;
+    }
+
+    public void setSelDepto(Departamento selDepto) {
+        this.selDepto = selDepto;
+    }
+
+    public DaoDepartamento getDdp() {
+        return ddp;
+    }
+
+    public void setDdp(DaoDepartamento ddp) {
+        this.ddp = ddp;
+    }
+
+    public List<Departamento> getLista() {
+        return lista;
+    }
+
+    public void setLista(List<Departamento> lista) {
+        this.lista = lista;
+    }
+    
 
     /**
      * Creates a new instance of BeanDepartamento
      */
-    public BeanDepartamento() {
+    public BeanDepartamento() throws Exception {
+        lista = ddp.listarDepartamentos();
     }
     
+    public void select()
+    {
+        d = selDepto;
+    }
 }
